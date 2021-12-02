7816b5b63eb0721dff888e27e05a25644da5c7b5
KimDabeen <83126140+Dabeen28@users.noreply.github.com>
2021-12-03 05:18:41 +0900

    Delete netmarble directory

--- netmarble/js/jq.js
+++
@@ -1,9 +0,0 @@
-// jqeury
-//     $(window).scroll(function(){
-//         var height = $(document).scrollTop();
-//         if(height > 0){
-//             $('.header').addClass("header-bg");
-//         }else if(height == 0){
-//             $('.header').removeClass("header-bg");
-//         }
-//     });
