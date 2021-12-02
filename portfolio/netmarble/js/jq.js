525a7dc32e1fb902f2ef8164fc4499779fd68816
KimDabeen <83126140+Dabeen28@users.noreply.github.com>
2021-12-03 05:20:58 +0900

    Add files via upload

---
+++ portfolio/netmarble/js/jq.js
@@ -0,0 +1,9 @@
+// jqeury
+//     $(window).scroll(function(){
+//         var height = $(document).scrollTop();
+//         if(height > 0){
+//             $('.header').addClass("header-bg");
+//         }else if(height == 0){
+//             $('.header').removeClass("header-bg");
+//         }
+//     });
